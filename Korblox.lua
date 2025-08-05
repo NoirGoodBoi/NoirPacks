215e98ac2643e1fddc23c4d1c2c3cb6961e5306d
NoirGoodBoi <phapxover@gmail.com>
2025-08-05 21:42:35 +0700

    Update and rename Ninja.lua to Korblox.lua

---
+++ Korblox.lua
@@ -0,0 +1,9 @@
+-- SETTINGS
+getgenv().Setting = {
+    ["Body"] = {
+        ["Korblox"] = true,
+        ["Headless"] = false,
+    },
+}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/khen791/script-khen/refs/heads/main/KorbloxAndHeadless.txt", true))()
