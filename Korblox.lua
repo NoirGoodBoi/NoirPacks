fa70bdd4b1f42b424fb144ec44a55a4cf894c99a
NoirGoodBoi <phapxover@gmail.com>
2025-09-17 17:06:37 +0700

    Update Korblox.lua

--- Korblox.lua
+++ Korblox.lua
@@ -1,9 +1,27 @@
--- SETTINGS
-getgenv().Setting = {
-    ["Body"] = {
-        ["Korblox"] = true,
-        ["Headless"] = false,
-    },
-}
+local function onCharacterAdded(character)
+    wait(1) 
+    
+    local rightLowerLeg = character:FindFirstChild("RightLowerLeg")
+    if rightLowerLeg then
+        rightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
+        rightLowerLeg.Transparency = 1
+    end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/khen791/script-khen/refs/heads/main/KorbloxAndHeadless.txt", true))()
+    local rightUpperLeg = character:FindFirstChild("RightUpperLeg")
+    if rightUpperLeg then
+        rightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+        rightUpperLeg.TextureID = "http://www.roblox.com/asset/?id=902843398"
+    end
+
+    local rightFoot = character:FindFirstChild("RightFoot")
+    if rightFoot then
+        rightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
+        rightFoot.Transparency = 1
+    end
+end
+
+game.Players.LocalPlayer.CharacterAdded:Connect(onCharacterAdded)
+
+if game.Players.LocalPlayer.Character then
+    onCharacterAdded(game.Players.LocalPlayer.Character)
+end
